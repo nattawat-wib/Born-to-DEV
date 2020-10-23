d0ea2fe0b9af0b2199eeca728ea974ca4132e52c
nattawat.wib <nattawat.wib@gmail.com>
2020-10-23 13:10:27 +0700

    scroll fixed

---
+++ index.js
@@ -0,0 +1,29 @@
+let sex = document.getElementById('');
+let weight = document.getElementById('weight').value;
+let height = document.getElementById('height');
+let age = document.getElementById('age');
+let healthBtn = document.getElementById('healthBtn');
+h__result = document.getElementById('h__result');
+
+
+function healthCalculators () {
+
+}
+
+let a = 1 && 2 ;
+alert(a);
+
+
+function myFunction() {
+    let weight = document.getElementById('male').value;
+    document.getElementById("h__result").innerHTML = weight;
+    alert(weight);
+  }
+
+
+
+
+console.log(weight);
+console.log(height);
+console.log(age);
+console.log();
