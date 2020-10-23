80ec34f5436e0bfb7f30821eba73cb57cc7716e0
nattawat.wib <nattawat.wib@gmail.com>
2020-10-23 15:14:38 +0700

    complete gpaDisplay function

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function healthCalculators() {
     document.getElementById("h__result").innerHTML = weight + height + age;
 }
 
-
 function loanCal() {
     let amount = document.getElementById('js-amount').value;
     let termYears = document.getElementById('js-termYears').value;
@@ -21,3 +20,23 @@ function loanCal() {
 
 loanDisplay = () => 
 document.getElementById('l__result').innerText = loanCal();
+
+
+//! GPA 
+
+function gpaCal() {
+    let math = document.getElementById('Math').value;
+    let thai = document.getElementById('Thai').value;
+    let english = document.getElementById('English').value;
+    let history = document.getElementById('History').value;
+    let sociology = document.getElementById('Sociology').value;
+    let health = document.getElementById('Health').value;
+
+    return math ;
+
+    // let result = (math + thai + english + history + sociology + health) / 6;
+    // return result;
+}
+
+gpaDisplay = () => 
+document.getElementById('e__result').innerText = document.getElementById('Math').value;;
