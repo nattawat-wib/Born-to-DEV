a10c298a6fa23a4669781191c7cbc2aed94661d2
nattawat.wib <nattawat.wib@gmail.com>
2020-10-23 14:20:59 +0700

    complete loanDisplay function

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-let sex = document.getElementById('');
-let weight = document.getElementById('weight').value;
-let height = document.getElementById('height');
-let age = document.getElementById('age');
-let healthBtn = document.getElementById('healthBtn');
-h__result = document.getElementById('h__result');
 
-
-function healthCalculators () {
-
+function healthCalculators() {
+    let weight = +document.getElementById('weight').value;
+    let height = +document.getElementById('height').value;
+    let age = +document.getElementById('age').value;
+    document.getElementById("h__result").innerHTML = weight + height + age;
 }
 
-let a = 1 && 2 ;
-alert(a);
 
+function checkLoan(aa) {
+    if (aa == 1) { 
+        alert('dd')
 
-function myFunction() {
-    let weight = document.getElementById('male').value;
-    document.getElementById("h__result").innerHTML = weight;
-    alert(weight);
-  }
+    }
+};
 
+function loanCal() {
+    let amount = document.getElementById('js-amount').value;
+    let termYears = document.getElementById('js-termYears').value;
+    let interestRate = document.getElementById('js-InterestRate').value;
 
+    checkLoan(amount, termYears, interestRate);
 
+    let result = ((amount * (interestRate/100) * 31) / termYears * 365);
 
-console.log(weight);
-console.log(height);
-console.log(age);
-console.log();
+    return result = result.toFixed(2);
+};
+
+loanDisplay = () => 
+document.getElementById('l__result').innerText = loanCal();
