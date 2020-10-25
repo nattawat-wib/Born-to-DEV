c7b6ceefe5d04055cf0f466f6940ecfcc11c8b10
nattawat.wib <nattawat.wib@gmail.com>
2020-10-25 10:12:28 +0700

    complete educate check bug function

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ function healthCal() {
     let height = +document.getElementById('height').value;
     let age = +document.getElementById('age').value;
     let result = 0;
-    
+
     if (document.getElementById('male').checked) {
-            result = 66 + (13.7 * weight) + (5 * height) - (6.8 * age);
+        result = 66 + (13.7 * weight) + (5 * height) - (6.8 * age);
     } else {
-            result = 665 + (9.6 * weight) + (1.8 * height) - (4.7 * age);
+        result = 665 + (9.6 * weight) + (1.8 * height) - (4.7 * age);
     }
 
     return result.toFixed(2);
 }
 
-function healthDisplay () {
+function healthDisplay() {
     document.getElementById("h__result").innerHTML = healthCal();
 }
 
@@ -22,8 +22,6 @@ function loanCal() {
     let amount = document.getElementById('js-amount').value;
     let termYears = document.getElementById('js-termYears').value;
     let interestRate = document.getElementById('js-InterestRate').value;
-
-    // return (`${amount} + ${termYears} + ${interestRate}`);
 
     let result = ((((amount * interestRate) / 100) * 31) / (termYears * 366));
     return result = result.toFixed(2);
@@ -42,8 +40,38 @@ function gpaCal() {
     let sociology = +document.getElementById('js-sociology').value;
     let health = +document.getElementById('js-health').value;
 
+    let answerArr = [math, thai, english, history, sociology, health];
+
+    answerArr.forEach(function(answer) {
+        if (answer < 0) {
+            document.getElementById('e__alert').style.display = 'block';  
+            document.getElementById('e__alert').innerText = "Can't input negative number";  
+            return result;
+        
+        } else if (answer > 4) {
+            document.getElementById('e__alert').style.display = 'block';  
+            document.getElementById('e__alert').innerText = "Can't input more than 4.00";  
+            return result; 
+    
+        } else if(answer !== 0 && 
+                answer !== 0.5 &&
+                answer !== 1 &&
+                answer !== 1.5 &&
+                answer !== 2 &&
+                answer !== 2.5 && 
+                answer !== 3 &&
+                answer !== 3.5 &&
+                answer !== 4 ) {
+            document.getElementById('e__alert').style.display = 'block';  
+            document.getElementById('e__alert').innerText = "Please input only 0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5 and 4";  
+            return result;  
+        } 
+    })
+
     let result = ((math + thai + english + history + sociology + health) / 6);
-    return result = result.toFixed(2);
+            
+            document.getElementById('e__alert').style.display = 'none';  
+            return result = result.toFixed(2);
 }
 
 gpaDisplay = () =>
@@ -51,6 +79,7 @@ gpaDisplay = () =>
 
 function gpaReset() {
     document.getElementById('e__result').innerText = 0;
+    document.getElementById('e__alert').style.display = 'none'; 
 
     document.getElementById('js-math').value = null;
     document.getElementById('js-thai').value = null;
